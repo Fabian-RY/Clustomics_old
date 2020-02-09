3589592bff185095762b617da8e70c1219433f10
Paulagomis <60719236+Paulagomis@users.noreply.github.com>
2020-02-09 17:45:40 +0100

    Add files via upload

--- plot1.py
+++ plot1.py
@@ -4,23 +4,21 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import seaborn 
 
-data_numbers = pd.DataFrame(pd.read_csv("input.csv", names=["gen1","gen2"]) )
-data_group= pd.DataFrame(pd.read_csv("nombre.csv",names=["grupo"]))
-
-data_numbers=pd.DataFrame([(1,1),(2,2),(3,3)])
-data_group=pd.DataFrame([0,1,2],columns=["grupo"])
-
-number_groups=len(set(data_group["grupo"]))
-
-colors=seaborn.color_palette(palette="Set1", n_colors=number_groups)
 
 
-color=[]
-for i in range(len(data_group)):
-    color.append(colors[data_group["grupo"][i]])
+def print_plot(list_info, list_group):
+    data_numbers=pd.DataFrame(list_info)
+    data_group=pd.DataFrame(list_group,columns=["grupo"])
 
-plt.scatter(x=data_numbers[data_numbers.columns[0]],y=data_numbers[data_numbers.columns[1]],color=color)
+    number_groups=len(set(data_group["grupo"]))
 
+    colors=seaborn.color_palette(palette="Set1", n_colors=number_groups)
+
+    color=[]
+    for i in range(len(data_group)):
+        color.append(colors[data_group["grupo"][i]])
+
+    plt.scatter(x=data_numbers[data_numbers.columns[0]],y=data_numbers[data_numbers.columns[1]],color=color)
 
 
 
